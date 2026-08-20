404a8ff1b1fbc7be9a64e296a8f9d603e910dad7
mmatsiko6-hub <mmatsiko6@icloud.com>
2026-08-20 13:41:04 +0300

    Add database dependency to essay CRUD operations

--- router.py
+++ router.py
@@ -1,23 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from database import get_db
 from schemas import EssayCreate, EssayUpdate
 from service import EssayService
+from sqlalchamy import Session
 
 service = EsssayService()
 router = APIRouter()
 
 @router.post("/essays")
-def create_essay(essay: EssayCreate):
-   return service.create_essay(essay)
+def create_essay(essay: EssayCreate, db: Session = Depends(get_db)):
+   return service.create_essay(essay, db)
    
 
 @router.get("/essays/{essay_id}")
-def get_essay(essay_id: int):
-    return service.get_essay_by_id(essay_id)
+def get_essay(essay_id: int, db: Session = Depends(get_db)):
+    return service.get_essay_by_id(essay_id, db)
 
 @router.put("/essays/{essay_id}")
-def update_essay(essay_id: int, essay: EssayUpdate):
-    return service.update_essay(essay_id, essay)
+def update_essay(essay_id: int, essay: EssayUpdate, db: Session = Depends(get_db)):
+    return service.update_essay(essay_id, essay, db)
 
 @router.delete("/essays/{essay_id}")
-def delete_essay(essay_id: int):
-    return service.delete_essay(essay_id)
+def delete_essay(essay_id: int, db: Session = Depends(get_db)):
+    return service.delete_essay(essay_id, db)
