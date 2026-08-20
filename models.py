deac24861209c9b51e4188e4094c581faca33783
mmatsiko6-hub <mmatsiko6@icloud.com>
2026-08-20 15:39:32 +0300

    Refactor Essay and Author models with type annotations
    Refactor Essay model to use SQLAlchemy's Mapped and mapped_column for type annotations. Update Author model to include foreign key relationship.

--- models.py
+++ models.py
@@ -7,17 +7,42 @@ from sqlalchemy import (
     Index,
     Integer,
     String,
+    ForeignKey,
+    
 )
+
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
+from enum import Status
 
 Base = declarative_base()
 
 class Essay(Base):
   __tablename__ = "essays"
   
-  essay_id = Column(Integer, primary_key = True, index = True )
-  essay_title = Column(String(100), nullable = False)
-  essay_author = Column(String(50), default = "ANONYMUS")
-  body = Column(Text, nullable = False)
-  created_at = Column(DATETIME(timezone = True), server_default = func.now())
-  status = Column(String(10), default = "draft", nullable = False)
+  essay_id: Mapped[int] = mapped_column(Integer, primary_key = True, index = True )
+  essay_title: Mapped[str] = mapped_column(String(100), nullable = False)
+  essay_author: Mapped[str] = mapped_column(String(50), default = "ANONYMUS")
+  body: Mapped[str] = mapped_Column(Text, nullable = False)
+  created_at: Mapped[DateTime] = Column(DATETIME(timezone = True), server_default = func.now())
+    
+  status: Mapped[DocumentStatus] = mapped_column(
+  Enum(DocumentStatus, name="documentstatus"),
+  nullable=False,
+  default=DocumentStatus.DRAFT
+          
+  authors = relationship("Author", back_populates = "essay")
+
+class Author(Base):
+    __tablename__ = "authors"
+    author_id = Column(Integer, primary_key = True, index = True)
+    essay_id = Column(Integer, ForeignKey(essays.essay_id))
+    author_name = Column(String(50))
+    essays = Column(Integer)
+
+
+    
+
+    
+
+    
