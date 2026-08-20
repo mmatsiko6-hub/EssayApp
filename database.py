3dd83e9473728eae8d99d4937b8c8a032cc1bd66
mmatsiko6-hub <mmatsiko6@icloud.com>
2026-08-20 11:07:54 +0300

    Refactor database connection to use get_database_url
    Fixed typos and redefined database.py

--- database.py
+++ database.py
@@ -5,16 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_database_url():
+    username = os.getenv("Postrage_username"),
+    password = os.getenv("Postrage_PASSWORD"),
+    mydb = os.getenv("Postrage_DSN")
+    return f"postgresql+psycopy2://{username}:{password}@{mydb}"
+    
 def connect_data_base():
-    def Database_URL():
-        username = os.getenv("Postrage_username"),
-        password = os.getenv("Postrage_PASSWORD"),
-        mydb = os.getenv("Postrage_DSN")
-        return f"postragesql+psycopy2://{username}:{password}@{mydb}"
-        
-    engine = create_engine(Database_URL())
-
+    engine = create_engine(get_database_url())
     SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-
     return engine, SessionLocal
-
